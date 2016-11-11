8c07266988085bd65679b29a2832fcec3acff063
Test Account (Nakov) <testnakov@nakov.com>
2016-11-11 22:38:58 +0200

    Added the Eucledian algorithm

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EucludeanAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            while (b != 0)
+            {
+                var oldB = b;
+                b = a % b;
+                a = oldB;
+            }
+            Console.WriteLine("GCD = {0}", a);
+        }
+    }
+}
